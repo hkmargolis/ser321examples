cab713a5889a8f2fc13289dfd40554e3e779827b
hkmargolis <hkmargolis@gmail.com>
2024-05-30 19:49:15 -0700

    Fixes 3 compile errors

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -268,7 +268,7 @@ class WebServer {
           builder.append("HTTP/1.1 200 OK\n");
           builder.append("Content-Type: text/html; charset=utf-8\n");
           builder.append("\n");
-          builder.append("D" + die1 + ": " + result1 + " D"+ die2 + ": ": + result2);
+          builder.append("D" + die1 + ": " + result1 + " D"+ die2 + ": " + result2);
         }
         else{
           // failure
